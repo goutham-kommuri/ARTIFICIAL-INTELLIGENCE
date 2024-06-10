08d1faaa289ab6c5d636672104816201bb827a75
GOUTHAMCHOWDARY <132541802+goutham-kommuri@users.noreply.github.com>
2024-06-10 09:48:41 +0530

    Update 1)8 PUZZLE PROBLEM.py

--- DAY1/1)8 PUZZLE PROBLEM.py
+++ DAY1/1)8 PUZZLE PROBLEM.py
@@ -1,108 +1,50 @@
-import copy
-from heapq import heappush, heappop
+import random
 
-n = 3
-row = [1, 0, -1, 0]
-col = [0, -1, 0, 1]
-
-class priorityQueue:
+class SlidingPuzzle:
     def __init__(self):
-        self.heap = []
+        self.state = list(range(1, 9)) + [0] 
+        random.shuffle(self.state) 
 
-    def push(self, k):
-        heappush(self.heap, k)
+    def __str__(self):
+        return '\n'.join([' '.join(map(str, self.state[i:i+3])) for i in range(0, 9, 3)])
 
-    def pop(self):
-        return heappop(self.heap)
+    def move(self, number):
+        empty_index = self.state.index(0)
+        number_index = self.state.index(number)
+        if (empty_index - number_index) in [-1, 1, -3, 3]:  
+            self.state[empty_index], self.state[number_index] = self.state[number_index], self.state[empty_index]
+        else:
+            print("Invalid move")
 
-    def empty(self):
-        return len(self.heap) == 0
+    def is_solved(self):
+        return self.state == [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
-class node:
-    def __init__(self, parent, mat, empty_tile_pos, cost, level):
-        self.parent = parent
-        self.mat = mat
-        self.empty_tile_pos = empty_tile_pos
-        self.cost = cost
-        self.level = level
-
-    def __lt__(self, nxt):
-        return self.cost < nxt.cost
-
-def calculateCost(mat, final) -> int:
-    count = 0
-    for i in range(n):
-        for j in range(n):
-            if mat[i][j] and mat[i][j] != final[i][j]:
-                count += 1
-    return count
-
-def newNode(mat, empty_tile_pos, new_empty_tile_pos, level, parent, final) -> node:
-    new_mat = copy.deepcopy(mat)
-    x1, y1 = empty_tile_pos
-    x2, y2 = new_empty_tile_pos
-    new_mat[x1][y1], new_mat[x2][y2] = new_mat[x2][y2], new_mat[x1][y1]
-    cost = calculateCost(new_mat, final)
-    return node(parent, new_mat, new_empty_tile_pos, cost, level)
-
-def printMatrix(mat):
-    for i in range(n):
-        for j in range(n):
-            print(f"{mat[i][j]} ", end=" ")
-        print()
-
-def isSafe(x, y):
-    return 0 <= x < n and 0 <= y < n
-
-def printPath(root):
-    if root is None:
-        return
-    printPath(root.parent)
-    printMatrix(root.mat)
-    print()
-
-def solve(initial, empty_tile_pos, final):
-    pq = priorityQueue()
-    cost = calculateCost(initial, final)
-    root = node(None, initial, empty_tile_pos, cost, 0)
-    pq.push(root)
-
-    while not pq.empty():
-        minimum = pq.pop()
-        if minimum.cost == 0:
-            printPath(minimum)
-            return
-
-        for i in range(4):
-            new_tile_pos = [minimum.empty_tile_pos[0] + row[i], minimum.empty_tile_pos[1] + col[i]]
-            if isSafe(new_tile_pos[0], new_tile_pos[1]):
-                child = newNode(minimum.mat, minimum.empty_tile_pos, new_tile_pos, minimum.level + 1, minimum, final)
-                pq.push(child)
-
-initial = [[1, 2, 3], [5, 6, 0], [7, 8, 4]]
-final = [[1, 2, 3], [5, 8, 6], [0, 7, 4]]
-empty_tile_pos = [1, 2]
-
-solve(initial, empty_tile_pos, final)
-
-OUTPUT:
-    >>> %Run lol.py
-1  2  3  
-5  6  0  
-7  8  4  
-
-1  2  3  
-5  0  6  
-7  8  4  
-
-1  2  3  
-5  8  6  
-7  0  4  
-
-1  2  3  
-5  8  6  
-0  7  4  
-
->>> 
+if __name__ == "__main__":
+    puzzle = SlidingPuzzle()
+    print("Initial State:")
+    print(puzzle)
+    while not puzzle.is_solved():
+        move = int(input("Enter number to move (0 to quit): "))
+        if move == 0:
+            break
+        puzzle.move(move)
+        print(puzzle)
+    if puzzle.is_solved():
+        print("Congratulations! Puzzle solved.")
+    else:
+        print("Quitting...")
+        
+ output       
+        >>> %Run lol.py
+Initial State:
+2 4 3
+8 0 5
+7 6 1
+Enter number to move (0 to quit): 1
+Invalid move
+2 4 3
+8 0 5
+7 6 1
+Enter number to move (0 to quit): 
 
 
